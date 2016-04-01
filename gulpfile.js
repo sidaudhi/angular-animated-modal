3469c0b63235ee6917ca25381ad5c6664b2b73cc
Siddharth Audhinarayanan <Siddharth Audhinarayanan>
2016-04-01 20:33:42 +0530

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var concatCss = require('gulp-concat-css');
+var nib = require('nib');
+
+gulp.task('default',['watch']);
+
+gulp.task('watch',['styles','scripts'],function(){
+  gulp.watch('./source/stylesheets/*.styl',['styles']);
+  gulp.watch('./source/javascript/*.js',['scripts']);
+});
+
+gulp.task('styles',['copy-sprites','copy-fonts'], function() {
+  return gulp.src([
+      './source/stylesheets/*.styl'
+    ])
+    .pipe(stylus({use:nib()}))
+    .pipe(concatCss('angular.animated.modal.css'))
+    .pipe(gulp.dest('./dist/stylesheets'));
+});
+
+gulp.task('copy-sprites', function() {
+   gulp.src('./source/images/*.{png,jpg}')
+   .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('copy-fonts', function() {
+   gulp.src('./source/fonts/*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('scripts',function(){
+  return gulp.src([
+    './source/javascript/*.js',
+  ])
+  .pipe(uglify())
+  .pipe(concat('angular.animated.modal.min.js'))
+  .pipe(gulp.dest('./dist/javascript'))
+})
---
+++ source/javascript/angular.animated.modal.js
@@ -0,0 +1,55 @@
+/**
+ * @license angular-circular-timepicker  version: 0.1.0
+ * Copyright 2016 sidaudhigithub.io, Inc. http://sidaudhi.github.io
+ * License: MIT
+ *
+ * @author        Siddharth Audhinarayanan
+ * @since         2016-Mar-26
+ */
+
+ var app = angular.module('angular.animated.modal',[]);
+ app.service('$animatedModal',['$compile','$rootScope','$document',function($compile,$rootScope,$document){
+   var $scope = $rootScope.$new();
+   return {
+     open: function(modalParams){
+       $scope.data = modalParams.data;
+       $scope.close = function(){
+         angular.element(document.getElementById('modal-container')).removeClass('visible');
+         setTimeout(function(){
+           angular.element(document.getElementById('modal-container')).remove();
+           var bodyStyles = {'height':'auto','overflow':'auto'};
+           angular.element(document).find('html').css(bodyStyles);
+           angular.element(document).find('body').css(bodyStyles);
+         },500)
+       }
+       var html = '<div id="modal-container" class="'+modalParams.theme+'">'
+                +     '<a class="anim-modal-close" ng-click="close()">&#x2716;</a>'
+                +     '<div id="modal-container-wrapper" class="anim-modal-wrapper">'
+                +       document.getElementById(modalParams.templateUrl).innerHTML
+                +     '</div>'
+                + '</div>';
+       var compiledHtml = $compile(html)($scope);
+       angular.element(document).find('body').append(compiledHtml);
+       angular.element(document.getElementById('modal-container')).css({'transform-origin':''+modalParams.coords[0]+'px '+modalParams.coords[1]+'px'});
+       setTimeout(function(){
+         angular.element(document.getElementById('modal-container')).addClass('visible');
+         var modalHeight = document.getElementById('modal-container-wrapper').offsetHeight+80;
+         var documentHeight = (window.innerHeight);
+         var heightRequired;
+         if(modalHeight > documentHeight){
+           heightRequired = modalHeight +'px';
+         }else{
+           heightRequired = documentHeight + 'px';
+         }
+         angular.element(document.getElementById('modal-container')).css({'height':heightRequired});
+         var bodyStyles = {'height':heightRequired,'overflow':'auto'};
+         angular.element(document).find('html').css(bodyStyles);
+         angular.element(document).find('body').css(bodyStyles);
+         if(modalHeight > documentHeight){
+           angular.element(document.getElementById('modal-container-wrapper')).addClass('larger-than-window');
+         }
+       },300)
+     },
+     close: $scope.close
+   }
+ }]);
